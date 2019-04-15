ff99bef8e01ec0cddb65436379c3692f3cda76ca
vlad-vm <vsve4nikov@mail.ru>
2019-04-16 00:53:01 +0300

    easy commit

--- FlaskApp/app/config.py
+++ FlaskApp/app/config.py
@@ -1,9 +1,26 @@
-CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
+# import os
+# CSRF_ENABLED = True
+# SECRET_KEY = 'you-will-never-guess'
+#
+#
+# basedir = os.path.abspath(os.path.dirname(__file__))
+#
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
-# OPENID_PROVIDERS = [
-#     { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
-#     { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
-#     { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
-#     { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
-#     { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
+    POSTS_PER_PAGE = 25
